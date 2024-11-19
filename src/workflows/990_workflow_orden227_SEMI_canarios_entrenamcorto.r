e4b37f4d533da3a3b670034042e7a6417b528b4c
mfernandez-code <mfernand@agro.uba.ar>
2024-11-19 03:25:39 +0000

    auto commit 2       20241119 032533

--- src/workflows/990_workflow_orden227_SEMI_canarios_entrenamcorto.r
+++ src/workflows/990_workflow_orden227_SEMI_canarios_entrenamcorto.r
@@ -304,7 +304,7 @@ TS_strategy_base8 <- function( pinputexps )
   param_local$final_train$clase_minoritaria <- c( "BAJA+1", "BAJA+2")
   param_local$final_train$training <- c(
     202106, 202105, 202104, 202103, 202102, 202101, 
-    202012, 202011, 202010, 202009, 202008, 202007, 
+    202012, 202011, 202010, 202009, 202008, 202007 
     # 202006  Excluyo por variables rotas
     #202005, 202004, 202003, 202002, 202001,
     #201912, 201911,
@@ -320,7 +320,7 @@ TS_strategy_base8 <- function( pinputexps )
 
   param_local$train$training <- c(
     202104, 202103, 202102, 202101, 
-    202012, 202011, 202010, 202009, 202008, 202007, 
+    202012, 202011, 202010, 202009, 202008, 202007 
     # 202006  Excluyo por variables rotas
     #202005, 202004, 202003, 202002, 202001,
     #201912, 201911,
